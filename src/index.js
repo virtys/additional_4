356988b931d1473ce10b449fb513ef3aff0344b1
virtys <sergei.blade14@gmail.com>
2017-09-24 17:49:48 +0300

    additional_4:feature done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  first = first.split('').reverse();
+  second = second.split('').reverse();
+  let result = [];
+
+  for (let i = 0; first[i] >= 0; i++) {
+        for (let j = 0; second[j] >= 0; j++) {
+            if (!result[i + j]) {
+                result[i + j] = 0;
+            }
+
+            result[i + j] += first[i] * second[j];
+        }
+    }
+
+    for (let i = 0; result[i] >= 0; i++) {
+        if (result[i] >= 10) {
+            if (!result[i + 1]) {
+                result[i + 1] = 0;
+            }
+
+            result[i + 1] += parseInt(result[i] / 10);
+            result[i] %= 10;
+        }
+    }
+
+    return result.reverse().join('');
 }
